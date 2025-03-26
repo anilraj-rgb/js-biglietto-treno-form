a10f7b4216e3b42411f7123cc8d5f79af7caff70
anilraj-rgb <anil.raj9727@gmail.com>
2025-03-26 16:27:25 +0100

    [UPD] do the price

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const costoKm = 0.21;
+let costoTot = 0; 
+const scontoMin = 0.20;
+const scontoAnz = 0.40;
+
+//Definisco il valore che prendo dal bottone clickato
+
+//Btn-1
+let sendBtnElement = document.getElementById("btn-send");
+
+//Btn-2
+let nullBtnElement = document.getElementById("btn-null");
+
+//Definisco le due variabile che conterranno rispettivamente i chilometri e l'età dell'utente
+
+//MILESTONE 1 VAR 
+let userKm;
+let userAge;
+
+//MILESTONE 2 VAR
+let userName;
+let userSurname;
